ed8441ab021675317d6bdc402fcfb807000fbd59
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-03-09 14:22:35 -0800

    Log shared items.
    Using code from react-native-shared-menu readme.
    Testing sharing a link from Safari I get:
    
     LOG  SHARED DATA [{"data": "https://www.apple.com/iphone-14-pro/", "mimeType": "text/plain"}] undefined
    
    Interesting that the mimeType isn't where the example claims it will be?
    
    And the message that I'm prompted to type by the system modal is nowhere to be found.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import '../wdyr';
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {LogBox} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Onyx from 'react-native-onyx';
 import {PortalProvider} from '@gorhom/portal';
+import ShareMenu from 'react-native-share-menu';
 import CustomStatusBar from './components/CustomStatusBar';
 import ErrorBoundary from './components/ErrorBoundary';
 import Expensify from './Expensify';
@@ -31,27 +32,59 @@ LogBox.ignoreLogs([
 
 const fill = {flex: 1};
 
-const App = () => (
-    <GestureHandlerRootView style={fill}>
-        <ComposeProviders
-            components={[
-                OnyxProvider,
-                SafeAreaProvider,
-                PortalProvider,
-                SafeArea,
-                LocaleContextProvider,
-                HTMLEngineProvider,
-                WindowDimensionsProvider,
-                KeyboardStateProvider,
-            ]}
-        >
-            <CustomStatusBar />
-            <ErrorBoundary errorMessage="NewExpensify crash caught by error boundary">
-                <Expensify />
-            </ErrorBoundary>
-        </ComposeProviders>
-    </GestureHandlerRootView>
-);
+const App = () => {
+    const [sharedData, setSharedData] = useState(null);
+    const [sharedMimeType, setSharedMimeType] = useState(null);
+    console.log('SHARED DATA', sharedData, sharedMimeType);
+
+    const handleShare = useCallback((item) => {
+        if (!item) {
+            return;
+        }
+
+        const {mimeType, data, extraData} = item;
+
+        setSharedData(data);
+        setSharedMimeType(mimeType);
+
+        // You can receive extra data from your custom Share View
+        console.log(extraData);
+    }, []);
+
+    useEffect(() => {
+        ShareMenu.getInitialShare(handleShare);
+    }, []);
+
+    useEffect(() => {
+        const listener = ShareMenu.addNewShareListener(handleShare);
+
+        return () => {
+            listener.remove();
+        };
+    }, []);
+
+    return (
+        <GestureHandlerRootView style={fill}>
+            <ComposeProviders
+                components={[
+                    OnyxProvider,
+                    SafeAreaProvider,
+                    PortalProvider,
+                    SafeArea,
+                    LocaleContextProvider,
+                    HTMLEngineProvider,
+                    WindowDimensionsProvider,
+                    KeyboardStateProvider,
+                ]}
+            >
+                <CustomStatusBar />
+                <ErrorBoundary errorMessage="NewExpensify crash caught by error boundary">
+                    <Expensify />
+                </ErrorBoundary>
+            </ComposeProviders>
+        </GestureHandlerRootView>
+    );
+};
 
 App.displayName = 'App';
 
