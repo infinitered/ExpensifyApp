f674839f1a12739a73ee6cfbbcab2dd92dc092d4
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-05-08 17:09:51 -0700

    Experimenting with different organization to see if it affects script. Nothing working so far.

--- src/components/SignInButtons/AppleSignIn.js
+++ src/components/SignInButtons/AppleSignIn.js
@@ -1,6 +1,6 @@
 /* eslint-disable rulesdir/prefer-early-return */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import {View} from 'react-native';
 import withLocalize, {withLocalizePropTypes} from '../withLocalize';
 import getUserLanguage from './getUserLanguage';
@@ -11,52 +11,54 @@ const $appleButtonContainerStyle = {
     width: 40, height: 40, marginRight: 20,
 };
 
+const htmlString = '<div style={{fontSize: "0"}} id="appleid-signin" data-mode="logo-only" data-color="white" data-border-radius="50" data-border="false" data-border-color="white" data-width="40" data-height="40" data-type="sign in" />';
+
+const createHtml = () => {__html: htmlString}
+
+const inner = {__html: htmlString}
+
+const DivOnly = () => {
+ return <div
+ dangerouslySetInnerHTML={inner}
+        />;
+};
+
 const AppleSignIn = (props) => {
-    React.useEffect(() => {
-        const localeCode = getUserLanguage();
-        const script = document.createElement('script');
-        script.src = `https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1//${localeCode}/appleid.auth.js`;
-        script.async = true;
+  const [isScriptReady, setIsScriptReady] = React.useState(false);
+  React.useLayoutEffect(() => {
+    const localeCode = getUserLanguage();
+    const script = document.createElement('script');
+    script.src = `https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1//${localeCode}/appleid.auth.js`;
+    script.async = true;
+    script.onload = () => setIsScriptReady(true);
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+      setIsScriptReady(false);
+    };
+  }, []);
 
-        const handleScriptLoad = () => {
-            window.AppleID.auth.init({
-                clientId: 'com.chat.expensify.chat',
-                scope: 'name email',
-                redirectURI: 'https://www.expensify.com/partners/apple/loginCallback',
-                state: 'state',
-                usePopup: true,
-            });
-        };
-        script.addEventListener('load', handleScriptLoad);
-        document.body.appendChild(script);
+  React.useEffect(() => {
+    if (isScriptReady) {
+    console.log("CALLING INIT EFFECT")
+      window.AppleID.auth.init({
+        clientId: 'com.chat.expensify.chat',
+        scope: 'name email',
+        redirectURI: 'https://www.expensify.com/partners/apple/loginCallback',
+        state: 'state',
+        usePopup: true,
+      });
+    }
+  }, [isScriptReady]);
 
-        return () => {
-            script.removeEventListener('load', handleScriptLoad);
-            document.body.removeChild(script);
-        };
-    }, []);
-
-    return (
-        <View
-            style={$appleButtonContainerStyle}
-            accessibilityRole="button"
-            accessibilityLabel={props.translate('common.signInWithApple')}
-        >
-            <div
-                style={{fontSize: '0'}}
-                id="appleid-signin"
-                data-mode="logo-only"
-                data-color="white"
-                data-border-radius="50"
-                data-border="false"
-                data-border-color="white"
-                data-width="40"
-                data-height="40"
-                data-type="sign in"
-            />
-        </View>
-
-    );
+  return (
+    <View
+      style={$appleButtonContainerStyle}
+      accessibilityRole="button"
+      accessibilityLabel={props.translate('common.signInWithApple')}>
+      <DivOnly />
+    </View>
+  );
 };
 
 AppleSignIn.displayName = 'AppleSignIn';
