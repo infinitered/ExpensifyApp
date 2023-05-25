76f5c5fc9473ff4b8cee4de631c39a1055f09864
Silas J. Matson <silas@infinite.red>
2023-05-25 11:17:56 -0700

    Refactor SIWA on android

--- src/components/SignInButtons/AppleSignIn/index.android.js
+++ src/components/SignInButtons/AppleSignIn/index.android.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {appleAuthAndroid} from '@invertase/react-native-apple-authentication';
-import Log from '../../../libs/Log';
 import ButtonBase from '../ButtonBase';
 import AppleLogoIcon from '../../../../assets/images/signIn/apple-logo.svg';
 import * as Session from '../../../libs/actions/Session';
@@ -12,32 +11,25 @@ const config = {
     scope: appleAuthAndroid.Scope.ALL,
 };
 
-function appleSignInRequest() {
+function signInWithApple() {
     appleAuthAndroid.configure(config);
-    return appleAuthAndroid
+    appleAuthAndroid
         .signIn()
-        .then((response) => response.id_token)
+        .then((response) => Session.beginAppleSignIn(response.id_token))
         .catch((e) => {
-            Log.error('Request to sign in with Apple failed. Error: ', e);
+            console.error('Request to sign in with Apple failed. Error: ', e);
             throw e;
         });
 }
 
-const AppleSignIn = () => {
-    const handleSignIn = () => {
-        appleSignInRequest()
-            .then((token) => Session.beginAppleSignIn(token))
-            .catch((e) => {
-                Log.error('Apple authentication failed', e);
-            });
-    };
+function AppleSignIn() {
     return (
         <ButtonBase
-            onPress={handleSignIn}
+            onPress={signInWithApple}
             icon={<AppleLogoIcon />}
         />
     );
-};
+}
 
 AppleSignIn.displayName = 'AppleSignIn';
 
--- src/components/SignInButtons/AppleSignIn/index.ios.js
+++ src/components/SignInButtons/AppleSignIn/index.ios.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import appleAuth from '@invertase/react-native-apple-authentication';
-import Log from '../../../libs/Log';
 import ButtonBase from '../ButtonBase';
 import AppleLogoIcon from '../../../../assets/images/signIn/apple-logo.svg';
 import * as Session from '../../../libs/actions/Session';
 
-function appleSignInRequest() {
+function signInWithApple() {
     return appleAuth
         .performRequest({
             requestedOperation: appleAuth.Operation.LOGIN,
@@ -13,32 +12,28 @@ function appleSignInRequest() {
             // FULL_NAME must come first, see https://github.com/invertase/react-native-apple-authentication/issues/293
             requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
         })
-        .then((response) =>
-            appleAuth.getCredentialStateForUser(response.user).then((credentialState) => {
-                if (credentialState !== appleAuth.State.AUTHORIZED) {
-                    Log.error('Authentication failed. Original response: ', response);
-                    throw new Error('Authentication failed');
-                }
-                return response.identityToken;
-            }),
-        );
+        .then((response) => Promise.all([response, appleAuth.getCredentialStateForUser(response.user)]))
+        .then(([response, credentialState]) => {
+            if (credentialState !== appleAuth.State.AUTHORIZED) {
+                throw new Error(`Could not find authorized credential.
+                  Original response: ${JSON.stringify(response, null, 2)}.
+                  Credential state: ${JSON.stringify(credentialState, null, 2)}
+                  `);
+            }
+            return response.identityToken;
+        })
+        .then((token) => Session.beginAppleSignIn(token))
+        .catch((e) => console.error('Apple credential lookup failed: ', e));
 }
 
-const AppleSignIn = () => {
-    const handleSignIn = () => {
-        appleSignInRequest()
-            .then((token) => Session.beginAppleSignIn(token))
-            .catch((e) => {
-                Log.error('Apple authentication failed', e);
-            });
-    };
+function AppleSignIn() {
     return (
         <ButtonBase
-            onPress={handleSignIn}
+            onPress={signInWithApple}
             icon={<AppleLogoIcon />}
         />
     );
-};
+}
 
 AppleSignIn.displayName = 'AppleSignIn';
 
