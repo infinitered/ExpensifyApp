c2e926d37cc9a427a0cbfa8d3a1380e661a5ceb8
Daniel Edwards <cdaniel.edwards@gmail.com>
2023-07-17 10:47:40 -0400

    Revert back Deeplinkredirectloadingindicator

--- src/components/DeeplinkWrapper/index.website.js
+++ src/components/DeeplinkWrapper/index.website.js
@@ -138,12 +138,7 @@ class DeeplinkWrapper extends PureComponent {
         }
 
         if (this.state.appInstallationCheckStatus === CONST.DESKTOP_DEEPLINK_APP_STATE.INSTALLED && this.shouldShowDeeplinkLoadingIndicator() && !this.state.shouldOpenLinkInBrowser) {
-            return (
-                <DeeplinkRedirectLoadingIndicator
-                    linkText="deeplinkWrapper.openLinkInBrowser"
-                    openLinkInBrowser={this.openLinkInBrowser}
-                />
-            );
+            return <DeeplinkRedirectLoadingIndicator openLinkInBrowser={this.openLinkInBrowser} />;
         }
 
         return this.props.children;
