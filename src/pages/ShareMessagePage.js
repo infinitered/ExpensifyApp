e69656720ad5c094bb59ae1e1d45133d13261ce8
Ryan Linton <ryanlntn@gmail.com>
2023-07-20 13:27:05 -0600

    unify flow and entry for iOS and Android

--- src/pages/ShareMessagePage.js
+++ src/pages/ShareMessagePage.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {Platform, Text, View} from 'react-native';
 import {ShareMenuReactView} from 'react-native-share-menu';
 
@@ -10,26 +10,25 @@ import TextInput from '../components/TextInput';
 import withLocalize, {withLocalizePropTypes} from '../components/withLocalize';
 // import additionalAppSetup from './src/setup';
 import OptionRowLHN from '../components/LHNOptionsList/OptionRowLHN';
-import Navigation from '../libs/Navigation/Navigation';
 import * as Report from '../libs/actions/Report';
+import Navigation from '../libs/Navigation/Navigation';
 import styles from '../styles/styles';
 
 function ShareMessagePage(props) {
     const reportID = props.route.params.reportID;
-    const [attachment, setAttachment] = useState(props.route.params.share);
-    const [message, setMessage] = useState('');
+    const attachment = props.route.params.share;
+    const source = attachment.data;
+    const uri = attachment.data;
+    const name = source.split('/').pop();
+    const type = attachment.mimeType;
+    const isTextShare = type === 'text/plain';
 
-    useEffect(() => {
-        if (Platform.OS === 'ios') {
-            ShareMenuReactView.data().then((share) => setAttachment(share && share.data[0]));
-        } else {
-            setAttachment(props.route.params.share);
-        }
-    }, [props.route.params.share]);
+    const [message, setMessage] = useState(isTextShare ? source : '');
 
-    // TODO: fix these
-    const close = Platform.select({ios: () => ShareMenuReactView.dismissExtension(), default: () => Navigation.dismissModal()});
-    const goBack = Platform.select({ios: () => console.log('TODO: fix goBack'), default: () => Navigation.goBack()});
+    const dismiss = Platform.select({
+        ios: () => ShareMenuReactView.dismissExtension(),
+        default: () => Navigation.dismissModal(),
+    });
 
     return (
         <ScreenWrapper
@@ -40,47 +39,45 @@ function ShareMessagePage(props) {
                 shouldShowBackButton={false}
                 shouldShowCloseButton
                 title={props.translate('newChatPage.shareToExpensify')}
-                onCloseButtonPress={close}
+                onCloseButtonPress={dismiss}
             />
             <Text style={[styles.textLabelSupporting, {paddingLeft: 24}]}>{props.translate('common.to')}</Text>
             <OptionRowLHN
                 isFocused
-                onSelectRow={goBack}
+                onSelectRow={Navigation.goBack}
                 reportID={reportID}
             />
             <View style={{padding: 24}}>
                 <TextInput
                     inputID="addAMessage"
+                    label={props.translate(isTextShare ? 'common.share' : 'moneyRequestConfirmationList.whatsItFor')}
                     name="addAMessage"
-                    label={props.translate('moneyRequestConfirmationList.whatsItFor')}
                     onChangeText={setMessage}
                     value={message}
                 />
             </View>
-            <View style={{padding: 24}}>
-                <Text style={styles.textLabelSupporting}>{props.translate('common.share')}</Text>
-                {attachment && (
-                    <View style={{borderRadius: 8, height: 200, marginTop: 8, overflow: 'hidden', width: '100%'}}>
-                        <AttachmentView source={attachment.data} />
-                    </View>
-                )}
-            </View>
+            {!isTextShare && (
+                <View style={{padding: 24}}>
+                    <Text style={styles.textLabelSupporting}>{props.translate('common.share')}</Text>
+                    {!!source && (
+                        <View style={{borderRadius: 8, height: 200, marginTop: 8, overflow: 'hidden', width: '100%'}}>
+                            <AttachmentView source={source} />
+                        </View>
+                    )}
+                </View>
+            )}
             <View style={{padding: 24}}>
                 <Button
                     success
                     pressOnEnter
                     text={props.translate('common.share')}
                     onPress={() => {
-                        const source = attachment.data;
-                        const uri = attachment.data;
-                        const name = source.split('/').pop();
-                        const type = attachment.mimeType;
-                        if (type === 'text/plain') {
-                            Report.addComment(reportID, source);
+                        if (isTextShare) {
+                            Report.addComment(reportID, message);
                         } else {
                             Report.addAttachment(reportID, {name, source, type, uri}, message);
                         }
-                        close();
+                        dismiss();
                     }}
                 />
             </View>
