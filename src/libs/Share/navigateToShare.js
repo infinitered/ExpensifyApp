b8059abf352434e4d40bc760c5d1d0977d022307
Ryan Linton <ryanlntn@gmail.com>
2023-07-28 09:37:09 -0600

    move guard logic to utility func

--- src/libs/Share/navigateToShare.js
+++ src/libs/Share/navigateToShare.js
@@ -1,11 +1,13 @@
 import isArray from 'lodash/isArray';
+import isEmpty from 'lodash/isEmpty';
 
 import ROUTES from '../../ROUTES';
 import Navigation from '../Navigation/Navigation';
 
+const hasNoShareData = (share) => !share || !share.data || isEmpty(share.data);
+
 const navigateToShare = (share) => {
-    if (!share || !share.data) return;
-    if (isArray(share.data) && share.data.length === 0) return;
+    if (hasNoShareData(share)) return;
     Navigation.isNavigationReady().then(() => {
         Navigation.navigate(ROUTES.NEW_GROUP);
         Navigation.setParams({share: isArray(share.data) ? share.data[0] : share});
