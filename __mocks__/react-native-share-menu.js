ece874297ded550edaf57bad7ebddc2ebb259e24
Ryan Linton <ryanlntn@gmail.com>
2023-07-27 18:08:04 -0600

    fix react-native-share-menu mock

--- __mocks__/react-native-share-menu.js
+++ __mocks__/react-native-share-menu.js
@@ -1,9 +1,13 @@
-export default {
+const ShareMenu = {
     addNewShareListener: jest.fn().mockReturnValue({remove: jest.fn()}),
     getInitialShare: jest.fn(),
     getSharedText: jest.fn(),
-    ShareMenuReactView: {
-        data: jest.fn(),
-        dismissExtension: jest.fn(),
-    },
 };
+
+const ShareMenuReactView = {
+    data: jest.fn().mockResolvedValue({}),
+    dismissExtension: jest.fn(),
+};
+
+export default ShareMenu;
+export {ShareMenuReactView};
