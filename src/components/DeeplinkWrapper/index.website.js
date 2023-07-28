5b88de275ac21478e4cb267e89d5cf7a0bd60def
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-07-28 14:30:20 -0700

    Messy proof of concept

--- src/components/DeeplinkWrapper/index.website.js
+++ src/components/DeeplinkWrapper/index.website.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import {PureComponent} from 'react';
+import {PureComponent, useState, useEffect, useLayoutEffect} from 'react';
 import Str from 'expensify-common/lib/str';
 import * as Browser from '../../libs/Browser';
 import ROUTES from '../../ROUTES';
@@ -7,19 +7,51 @@ import * as App from '../../libs/actions/App';
 import CONST from '../../CONST';
 import CONFIG from '../../CONFIG';
 import shouldShowDeeplink from '../../libs/Navigation/shouldShowDeeplink';
+import navigationRef from '../../libs/Navigation/navigationRef';
+import Navigation from '../../libs/Navigation/Navigation';
 
 const propTypes = {
     /** Children to render. */
     children: PropTypes.node.isRequired,
 };
 
-class DeeplinkWrapper extends PureComponent {
-    componentDidMount() {
-        CONFIG.ENVIRONMENT = CONST.ENVIRONMENT.STAGING;
-        if (!this.isMacOSWeb() || CONFIG.ENVIRONMENT === CONST.ENVIRONMENT.DEV) {
-            return;
-        }
+function isMacOSWeb() {
+    return !Browser.isMobile() &&
+    typeof navigator === 'object' &&
+    typeof navigator.userAgent === 'string' &&
+    /Mac/i.test(navigator.userAgent) &&
+    !/Electron/i.test(navigator.userAgent);
+}
 
+console.log("HI")
+function DeeplinkWrapper({children}) {
+    const [currentScreen, setCurrentScreen] = useState();
+    const [currentPath, setCurrentPath] = useState();
+    useEffect(() => {
+        Navigation.isNavigationReady().then(() => {
+          console.log("TRYING TO ADD");
+
+          console.log("REF? ", navigationRef, navigationRef.current)
+          // get initial route
+          if (currentScreen === undefined) {
+            const initialRoute = navigationRef.current.getCurrentRoute()
+            console.log("INIT", initialRoute)
+            setCurrentScreen(initialRoute.name)
+            setCurrentPath(initialRoute.path)
+          }
+          
+          navigationRef.current.addListener('state', (event) => {
+              console.log("EVENT: ", JSON.stringify(event, null, 2))
+              // accessing routes here should be in a navigation lib fn in case the state shape changes in the future
+              // can't remember if I want first or last item in list
+              setCurrentScreen(event.data.state.routes[0].name)
+              setCurrentPath(event.data.state.routes[0].path)
+          })
+        });
+    }, []);
+    useEffect(() => {
+        CONFIG.ENVIRONMENT = CONST.ENVIRONMENT.STAGING;
+        console.log("Current route", currentScreen, currentPath);
         const onDenyListRoute = !shouldShowDeeplink();
 
         if (onDenyListRoute) {
@@ -35,15 +67,8 @@ class DeeplinkWrapper extends PureComponent {
             return;
         }
         App.beginDeepLinkRedirect();
-    }
-
-    isMacOSWeb() {
-        return !Browser.isMobile() && typeof navigator === 'object' && typeof navigator.userAgent === 'string' && /Mac/i.test(navigator.userAgent) && !/Electron/i.test(navigator.userAgent);
-    }
-
-    render() {
-        return this.props.children;
-    }
+    }, [currentScreen]);
+    return children;
 }
 
 DeeplinkWrapper.propTypes = propTypes;
