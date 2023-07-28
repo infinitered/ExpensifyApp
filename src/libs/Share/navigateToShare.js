189d9fba905c47d303c2640949473ff4e3c9ec66
Ryan Linton <ryanlntn@gmail.com>
2023-07-28 10:08:14 -0600

    move up share data formatting as well

--- src/libs/Share/navigateToShare.js
+++ src/libs/Share/navigateToShare.js
@@ -4,13 +4,24 @@ import isEmpty from 'lodash/isEmpty';
 import ROUTES from '../../ROUTES';
 import Navigation from '../Navigation/Navigation';
 
+const formatShareData = (shared) => {
+    const share = isArray(shared.data) ? shared.data[0] : shared;
+    return {
+        isTextShare: share.mimeType === 'text/plain',
+        name: share.data.split('/').pop(),
+        source: share.data,
+        type: share.mimeType,
+        uri: share.data,
+    };
+};
+
 const hasNoShareData = (share) => !share || !share.data || isEmpty(share.data);
 
 const navigateToShare = (share) => {
     if (hasNoShareData(share)) return;
     Navigation.isNavigationReady().then(() => {
         Navigation.navigate(ROUTES.NEW_GROUP);
-        Navigation.setParams({share: isArray(share.data) ? share.data[0] : share});
+        Navigation.setParams({share: formatShareData(share)});
     });
 };
 
