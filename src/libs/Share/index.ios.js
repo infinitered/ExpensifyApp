cc317ba4d5c2eccecddb62927ac73dd97889cec0
Ryan Linton <ryanlntn@gmail.com>
2023-07-28 09:49:04 -0600

    move dismiss to Share lib reduce use of platform

--- src/libs/Share/index.ios.js
+++ src/libs/Share/index.ios.js
@@ -2,6 +2,8 @@ import {ShareMenuReactView} from 'react-native-share-menu';
 import Navigation from '../Navigation/Navigation';
 import navigateToShare from './navigateToShare';
 
+const dismiss = () => ShareMenuReactView.dismissExtension();
+
 const registerListener = () => {
     Navigation.isNavigationReady().then(() => {
         ShareMenuReactView.data().then(navigateToShare);
@@ -10,5 +12,6 @@ const registerListener = () => {
 };
 
 export default {
+    dismiss,
     registerListener,
 };
