1cb70f3decd912cce0232707ac94a92f384ccb99
Ryan Linton <ryanlntn@gmail.com>
2023-08-16 15:16:18 -0600

    refactor offline clean up

--- src/libs/Share/index.js
+++ src/libs/Share/index.js
@@ -3,6 +3,7 @@ import Navigation from '../Navigation/Navigation';
 const dismiss = () => Navigation.dismissModal();
 
 export default {
+    cleanUpActions: () => [],
     dismiss,
     isShareExtension: false,
     openApp: () => {},
