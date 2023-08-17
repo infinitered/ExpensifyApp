a623fd701e86e35b3d1e451200312cd9c93e1e9c
Ryan Linton <ryanlntn@gmail.com>
2023-08-17 15:13:49 -0600

    move up app group directory handling

--- src/CONST.ts
+++ src/CONST.ts
@@ -824,6 +824,7 @@ const CONST = {
         // Video MimeTypes allowed by iOS photos app.
         VIDEO: /\.(mov|mp4)$/,
     },
+    IOS_APP_GROUP: 'group.com.chat.expensify.chat',
     IOS_CAMERAROLL_ACCESS_ERROR: 'Access to photo library was denied',
     ADD_PAYMENT_MENU_POSITION_Y: 226,
     ADD_PAYMENT_MENU_POSITION_X: 356,
