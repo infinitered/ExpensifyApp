5a1400f869501a2f1e3caf56241ef6291239b625
Ryan Linton <ryanlntn@gmail.com>
2023-09-11 14:48:11 -0600

    add migration to move onyx to app group dir on ios

--- src/libs/actions/Session/clearCache/index.native.ts
+++ src/libs/actions/Session/clearCache/index.native.ts
@@ -1,5 +1,7 @@
-import {CachesDirectoryPath, unlink} from 'react-native-fs';
+import {CachesDirectoryPath, DocumentDirectoryPath, unlink} from 'react-native-fs';
 import ClearCache from './types';
+
+console.log({DocumentDirectoryPath});
 
 // `unlink` is used to delete the caches directory
 const clearStorage: ClearCache = () => unlink(CachesDirectoryPath);
---
+++ src/libs/migrations/MoveToAppGroup.js
@@ -0,0 +1,16 @@
+/* eslint-disable @lwc/lwc/no-async-await */
+import {Platform} from 'react-native';
+import {DocumentDirectoryPath, exists, moveFile, pathForGroup} from 'react-native-fs';
+import CONST from '../../CONST';
+
+export default async function () {
+    if (!Platform.OS === 'ios') return;
+    const appGroupPath = await pathForGroup(CONST.IOS_APP_GROUP);
+    const hasAppGroupDB = await exists(`${appGroupPath}/OnyxDB`);
+    const hasDocsDirDB = await exists(`${DocumentDirectoryPath}/OnyxDB`);
+    if (!hasAppGroupDB && hasDocsDirDB) {
+        await moveFile(`${DocumentDirectoryPath}/OnyxDB`, `${appGroupPath}/OnyxDB`);
+        await moveFile(`${DocumentDirectoryPath}/OnyxDB-shm`, `${appGroupPath}/OnyxDB-shm`);
+        await moveFile(`${DocumentDirectoryPath}/OnyxDB-wal`, `${appGroupPath}/OnyxDB-wal`);
+    }
+}
