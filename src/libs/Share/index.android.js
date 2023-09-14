310da714ced7c05ff3c1bca99a39100f44d012b4
Daniel Edwards <cdaniel.edwards@gmail.com>
2023-09-14 11:20:27 -0400

    Removed additional Navigation step

--- src/libs/Share/index.android.js
+++ src/libs/Share/index.android.js
@@ -2,11 +2,9 @@ import ShareMenu from 'react-native-share-menu';
 import Navigation from '../Navigation/Navigation';
 import isShareExtension from './isShareExtension';
 import navigateToShare from './navigateToShare';
-import ROUTES from '../../ROUTES';
 
 const dismiss = (reportID) => {
-    Navigation.dismissModal();
-    Navigation.navigate(ROUTES.getReportRoute(reportID));
+    Navigation.dismissModal(reportID);
 };
 
 const registerListener = () => {
