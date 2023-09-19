7611d9efb77a28710761529607a733343a524250
Ryan Linton <ryanlntn@gmail.com>
2023-09-18 21:08:31 -0600

    initial react-native-share-menu setup

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
  * @format
  */
 
-import {enableLegacyWebImplementation} from 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import {enableLegacyWebImplementation} from 'react-native-gesture-handler';
 import App from './src/App';
 import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
 
 enableLegacyWebImplementation(true);
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
+AppRegistry.registerComponent('ShareMenuModuleComponent', () => App);
 additionalAppSetup();
