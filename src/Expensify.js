4508dddb150ebd7a95099353cca0caf54441ebb0
Ryan Linton <ryanlntn@gmail.com>
2023-09-20 09:48:05 -0600

    fix botched imports

--- src/Expensify.js
+++ src/Expensify.js
@@ -4,31 +4,39 @@ import React, {useCallback, useEffect, useLayoutEffect, useMemo, useRef, useStat
 import {AppState, Linking} from 'react-native';
 import Onyx, {withOnyx} from 'react-native-onyx';
 import ShareMenu from 'react-native-share-menu';
-import * as Report from './libs/actions/Report';
-import BootSplash from './libs/BootSplash';
-import * as ActiveClientManager from './libs/ActiveClientManager';
+import _ from 'underscore';
 import ONYXKEYS from './ONYXKEYS';
 import ROUTES from './ROUTES';
 import ConfirmModal from './components/ConfirmModal';
-import compose from './libs/compose';
-import withLocalize, {withLocalizePropTypes} from './components/withLocalize';
-import * as User from './libs/actions/User';
-import NetworkConnection from './libs/NetworkConnection';
-import Navigation from './libs/Navigation/Navigation';
-import PopoverReportActionContextMenu from './pages/home/report/ContextMenu/PopoverReportActionContextMenu';
-import * as ReportActionContextMenu from './pages/home/report/ContextMenu/ReportActionContextMenu';
-import SplashScreenHider from './components/SplashScreenHider';
+import DeeplinkWrapper from './components/DeeplinkWrapper';
+import EmojiPicker from './components/EmojiPicker/EmojiPicker';
+import GrowlNotification from './components/GrowlNotification';
 import KeyboardShortcutsModal from './components/KeyboardShortcutsModal';
 import AppleAuthWrapper from './components/SignInButtons/AppleAuthWrapper';
-import EmojiPicker from './components/EmojiPicker/EmojiPicker';
+import SplashScreenHider from './components/SplashScreenHider';
+import UpdateAppModal from './components/UpdateAppModal';
+import withLocalize, {withLocalizePropTypes} from './components/withLocalize';
+import * as ActiveClientManager from './libs/ActiveClientManager';
+import BootSplash from './libs/BootSplash';
+import * as Growl from './libs/Growl';
+import Log from './libs/Log';
+import Navigation from './libs/Navigation/Navigation';
+import NavigationRoot from './libs/Navigation/NavigationRoot';
+import NetworkConnection from './libs/NetworkConnection';
+import PushNotification from './libs/Notification/PushNotification';
+import StartupTimer from './libs/StartupTimer';
+import Visibility from './libs/Visibility';
 import * as EmojiPickerAction from './libs/actions/EmojiPickerAction';
-import DeeplinkWrapper from './components/DeeplinkWrapper';
+import * as Report from './libs/actions/Report';
+import * as User from './libs/actions/User';
+import compose from './libs/compose';
+import migrateOnyx from './libs/migrateOnyx';
+import PopoverReportActionContextMenu from './pages/home/report/ContextMenu/PopoverReportActionContextMenu';
+import * as ReportActionContextMenu from './pages/home/report/ContextMenu/ReportActionContextMenu';
 
 // This lib needs to be imported, but it has nothing to export since all it contains is an Onyx connection
 // eslint-disable-next-line no-unused-vars
-import UnreadIndicatorUpdater from './libs/UnreadIndicatorUpdater';
 // eslint-disable-next-line no-unused-vars
-import subscribePushNotification from './libs/Notification/PushNotification/subscribePushNotification';
 
 Onyx.registerLogger(({level, message}) => {
     if (level === 'alert') {
