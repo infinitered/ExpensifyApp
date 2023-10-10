cc33d667b9d41b550bf0a9dd1d5ba01c591bd3f9
Ryan Linton <ryanlntn@gmail.com>
2023-10-10 13:01:18 -0600

    don't need ShareMenuModuleComponent anymore either

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
 import {enableLegacyWebImplementation} from 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
 import App from './src/App';
 import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
 
 enableLegacyWebImplementation(true);
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
-AppRegistry.registerComponent('ShareMenuModuleComponent', () => App);
 additionalAppSetup();
