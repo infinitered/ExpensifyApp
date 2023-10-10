1ce636e65b9c7bc89272176e5515f4539bbb9227
Ryan Linton <ryanlntn@gmail.com>
2023-10-10 13:25:57 -0600

    remove now unused onyx keys

--- src/ONYXKEYS.ts
+++ src/ONYXKEYS.ts
@@ -1,8 +1,7 @@
-import {OnyxUpdate} from 'react-native-onyx';
 import {ValueOf} from 'type-fest';
-import CONST from './CONST';
-import * as OnyxTypes from './types/onyx';
 import DeepValueOf from './types/utils/DeepValueOf';
+import * as OnyxTypes from './types/onyx';
+import CONST from './CONST';
 
 /**
  * This is a file containing constants for all the top level keys in our store
@@ -30,14 +29,8 @@ const ONYXKEYS = {
     /** Note: These are Persisted Requests - not all requests in the main queue as the key name might lead one to believe */
     PERSISTED_REQUESTS: 'networkRequestQueue',
 
-    /** Requests persisted from the share extension. Kept separate since they are persisted from a different process. */
-    SHARE_PERSISTED_REQUESTS: 'shareExtensionNetworkRequestQueue',
-
     /** URLs to temp files queued for deletion */
     TEMP_FILES_TO_DELETE: 'tempFilesToDelete',
-
-    /** Onyx updates from a response, or success or failure data from a request. */
-    QUEUED_ONYX_UPDATES: 'queuedOnyxUpdates',
 
     /** Stores current date */
     CURRENT_DATE: 'currentDate',
@@ -316,8 +309,6 @@ type OnyxValues = {
     [ONYXKEYS.DEVICE_ID]: string;
     [ONYXKEYS.IS_SIDEBAR_LOADED]: boolean;
     [ONYXKEYS.PERSISTED_REQUESTS]: OnyxTypes.Request[];
-    [ONYXKEYS.SHARE_PERSISTED_REQUESTS]: OnyxTypes.Request[];
-    [ONYXKEYS.QUEUED_ONYX_UPDATES]: OnyxUpdate[];
     [ONYXKEYS.CURRENT_DATE]: string;
     [ONYXKEYS.CREDENTIALS]: OnyxTypes.Credentials;
     [ONYXKEYS.IOU]: OnyxTypes.IOU;
@@ -436,4 +427,4 @@ type OnyxValues = {
 };
 
 export default ONYXKEYS;
-export type {OnyxCollectionKey, OnyxKey, OnyxValues};
+export type {OnyxKey, OnyxCollectionKey, OnyxValues};
